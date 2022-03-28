40e6c13163d62a3eb4c497f445307199f64f97fd
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2022-03-28 10:02:22 -0500

    added revamped project explorer backend and frontend

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,6 @@
 export const projects = [
   {
-    title: "Frontend developer website",
+    title: "Frontend developer",
     description:
       "VanillaJs and Bootstrap 5 was used to build this web app for people who want to learn frontend web development.",
     image: "/images/front.PNG",
@@ -10,24 +10,34 @@ export const projects = [
     id: 0,
   },
   {
-    title: "Project Explorer",
+    title: "Project Explorer API",
     description:
-      "Project Explorer is a repository for final year students to submit projects and search projects submitted by others to learn from. Students can signup or login via the regular signup/login procedures, or do that via Facebook or Google.",
-    image: "/images/explorer.png",
-    tags: ["MongoDB", "Express", "React", "Node"],
-    source: "https://jide-explorer.herokuapp.com/",
+      "The backend of the Project Explorer project, repository for final year students to submit projects and search projects submitted by others to learn from. Students can signup or login via the regular signup/login procedures, or do so via Facebook or Google.",
+    image: "/images/explorer-backend.jpg",
+    tags: ["MongoDB", "Express", "Passport", "Node"],
+    source: "https://jide-explorer.herokuapp.com",
     visit: "https://github.com/Babajide777/project-explorer",
     id: 1,
   },
   {
-    title: "E-Commerce",
+    title: "Project Explorer Client",
+    description:
+      "The frontend of the Project Explorer project, a repository for final year students to submit projects and search projects submitted by others to learn from. Students can signup or login via the regular signup/login procedures, or do so via Facebook or Google.",
+    image: "/images/explorer.jpg",
+    tags: ["React", "Node", "Bootstrap", "React-Bootstrap"],
+    source: "https://babajide-project-explorer.netlify.app",
+    visit: "https://github.com/Babajide777/project-explorer-client",
+    id: 2,
+  },
+  {
+    title: "King Jesus E-Commerce",
     description:
       "This is a fully functional ECommerce website with cart, payment, search, emailing, and Admin section all working properly. The Repo is private as the application is for a client.",
     image: "/images/kingJesus.PNG",
     tags: ["JavaScript", "MongoDB", "Express", "Node", "Flutterwave"],
     source: "https://kingjesus-ecommerce.herokuapp.com/",
     visit: "#",
-    id: 2,
+    id: 3,
   },
 ];
 
