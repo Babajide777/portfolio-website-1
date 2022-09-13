ea1142b6fe9e0b5e13ffa98ccbcfa404d74036dc
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2022-09-12 22:47:05 -0500

    September 2022 portfolio update

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,12 +1,12 @@
 export const projects = [
   {
-    title: "Frontend developer",
+    title: "Animal Hospital Website",
     description:
-      "VanillaJs and Bootstrap 5 was used to build this web app for people who want to learn frontend web development.",
-    image: "/images/front.PNG",
-    tags: ["VanillaJs", "Bootstrap"],
-    source: "https://jide-frontend-bootstrap.netlify.app/",
-    visit: "https://github.com/Babajide777/BOOTSTRAP-BOOTCAMP-WEBSITE",
+      "This is a website for a vetinary clinic that specialize in cats",
+    image: "/images/cat-vet.jpg",
+    tags: ["NextJs", "Styled Components", "SwiperJS", "NodeJs"],
+    source: "https://babajide-omar-animal-hospital.netlify.app/",
+    visit: "#",
     id: 0,
   },
   {
@@ -40,18 +40,51 @@ export const projects = [
     id: 3,
   },
   {
+    title: "King Jesus E-Commerce Admin",
+    description: "Admin Section for king Jesus E-Commerce website",
+    image: "/images/king-admin.jpg",
+    tags: ["JavaScript", "MongoDB", "Express", "Node", "Flutterwave"],
+    source: "https://kingjesus-ecommerce.herokuapp.com/admin",
+    visit: "#",
+    id: 4,
+  },
+  {
     title: "Eventis",
     description: "Landing page for an upcoming video sharing website",
     image: "/images/eventis.jpg",
     tags: ["CSS3", "HTML5"],
     source: "https://www.eventis.me/",
     visit: "#",
-    id: 4,
+    id: 5,
+  },
+  {
+    title: "Ever Tech",
+    description:
+      "I was part of the team that built the Ever Tech website from scratch",
+    image: "/images/ever-tech.png",
+    tags: ["NextJs", "TypeScript", "ChakraUI", "StyledComponents"],
+    source: "https://services.ever.co/",
+    visit: "#",
+    id: 6,
+  },
+  {
+    title: "Ever Website",
+    description: "I was part of the team that built the Ever website",
+    image: "/images/ever.png",
+    tags: ["NextJs", "TypeScript", "ChakraUI", "StyledComponents"],
+    source: "https://ever.co/",
+    visit: "#",
+    id: 6,
   },
 ];
 
 export const TimeLineData = [
   { year: 2020, text: "Started my Web development journey" },
   { year: 2021, text: "Completed EdConnect bootcamp" },
-  { year: 2021, text: "Worked as a freelance fullstack developer" },
+  {
+    year: 2021,
+    text: "Worked as a freelance fullstack developer at codementor and Upwork",
+  },
+  { year: 2021, text: "Worked with Eventis as a Backend Engineer" },
+  { year: 2022, text: "Worked with Ever as a Frontend Web Developer" },
 ];
